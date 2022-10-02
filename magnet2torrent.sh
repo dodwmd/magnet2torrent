58fab3f66a346dbfa4b248e02a50ee6aa5cc1c84
Michael Dodwell <1372930+dodwmd@users.noreply.github.com>
2022-10-02 23:36:11 +0000

    cleanup

--- magnet2torrent.sh
+++ magnet2torrent.sh
@@ -8,21 +8,22 @@ DIRECTORY=/volume1/volume1/Downloads/blackhole
 
 if pidof -x $(basename $0) > /dev/null; then
   for p in $(pidof -x $(basename $0)); do
-    if [ $p -ne $$ ]; then
-      echo "Script $0 is already running: exiting"
+    if [ ${p} -ne $$ ]; then
+      echo "Script ${0} is already running: exiting"
       exit
     fi
   done
 fi
 
-SAVEIFS=$IFS
+SAVEIFS=${IFS}
 IFS=$(echo -en "\n\b")
 
 for f in ${DIRECTORY}/*.magnet; do
-  echo "converting $f"
-  docker run -u $(id -u):$(id -g) -v ${DIRECTORY}:/torrents -v ${DIRECTORY}/.cache/aria2:/.cache/aria2--rm -ti dodwmd/aria2c:latest /usr/bin/aria2c -d /torrents --bt-metadata-only=true --bt-save-metadata=true --listen-port=6881 --enable-dht --dht-listen-port=6881 --enable-dht6=false $(cat "$f")
-  echo "deleting $f"
-  rm -f "$f"
+  [[ -e "${f}" ]] || continue
+  echo "converting ${f}"
+  docker run -u $(id -u):$(id -g) -v ${DIRECTORY}:/torrents -v ${DIRECTORY}/.cache/aria2:/.cache/aria2--rm -ti dodwmd/aria2c:latest /usr/bin/aria2c -d /torrents --bt-metadata-only=true --bt-save-metadata=true --listen-port=6881 --enable-dht --dht-listen-port=6881 --enable-dht6=false $(cat "${f}")
+  echo "deleting ${f}"
+  rm -f "${f}"
 done
 
-IFS=$SAVEIFS
+IFS=${SAVEIFS}
