8727eac84f9155d0ea5f7840bc6aafb6f791eca6
Michael Dodwell <1372930+dodwmd@users.noreply.github.com>
2022-10-02 11:20:10 +0000

    add readme and remove ipv6 support

---
+++ README.md
@@ -0,0 +1,1 @@
+Easy method to turn blackhole directory with magnet files into blackhole directory with torrent files
--- magnet2torrent.sh
+++ magnet2torrent.sh
@@ -20,7 +20,7 @@ IFS=$(echo -en "\n\b")
 
 for f in ${DIRECTORY}/*.magnet; do
   echo "converting $f"
-  docker run -u $(id -u):$(id -g) -v ${DIRECTORY}:/torrents -v ${DIRECTORY}/.cache/aria2c:/.cache/aria2c--rm -ti dodwmd/aria2c:latest /usr/bin/aria2c -d /torrents --bt-metadata-only=true --bt-save-metadata=true --listen-port=6881 --enable-dht --dht-listen-port=6881 $(cat "$f")
+  docker run -u $(id -u):$(id -g) -v ${DIRECTORY}:/torrents -v ${DIRECTORY}/.cache/aria2c:/.cache/aria2c--rm -ti dodwmd/aria2c:latest /usr/bin/aria2c -d /torrents --bt-metadata-only=true --bt-save-metadata=true --listen-port=6881 --enable-dht --dht-listen-port=6881 --enable-dht6=false $(cat "$f")
   echo "deleting $f"
   rm -f "$f"
 done
