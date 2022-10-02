56bc75ee4fc06033040a929e09b2851deb06090d
Michael Dodwell <1372930+dodwmd@users.noreply.github.com>
2022-10-02 23:27:27 +0000

    fix typos

--- magnet2torrent.sh
+++ magnet2torrent.sh
@@ -4,7 +4,7 @@
 
 DIRECTORY=/volume1/volume1/Downloads/blackhole
 
-[[ ! -d ${DIRECTORY}}/.cache/aria2c]] && mkdir -p ${DIRECTORY}}/.cache/aria2c
+[[ ! -d "${DIRECTORY}/.cache/aria2" ]] && mkdir -p "${DIRECTORY}/.cache/aria2"
 
 if pidof -x $(basename $0) > /dev/null; then
   for p in $(pidof -x $(basename $0)); do
@@ -20,7 +20,7 @@ IFS=$(echo -en "\n\b")
 
 for f in ${DIRECTORY}/*.magnet; do
   echo "converting $f"
-  docker run -u $(id -u):$(id -g) -v ${DIRECTORY}:/torrents -v ${DIRECTORY}/.cache/aria2c:/.cache/aria2c--rm -ti dodwmd/aria2c:latest /usr/bin/aria2c -d /torrents --bt-metadata-only=true --bt-save-metadata=true --listen-port=6881 --enable-dht --dht-listen-port=6881 --enable-dht6=false $(cat "$f")
+  docker run -u $(id -u):$(id -g) -v ${DIRECTORY}:/torrents -v ${DIRECTORY}/.cache/aria2:/.cache/aria2--rm -ti dodwmd/aria2c:latest /usr/bin/aria2c -d /torrents --bt-metadata-only=true --bt-save-metadata=true --listen-port=6881 --enable-dht --dht-listen-port=6881 --enable-dht6=false $(cat "$f")
   echo "deleting $f"
   rm -f "$f"
 done
